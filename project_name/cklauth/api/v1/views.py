d9f6ec9684e58b5b4b4a9eb2b22763e42f248baa
Joao Ricardo <joaoricardo000@gmail.com>
2017-02-28 15:40:53 -0300

    App cklauth with default urls/endpoints

---
+++ project_name/cklauth/api/v1/views.py
@@ -0,0 +1,6 @@
+from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+from rest_auth.registration.views import SocialLoginView
+
+
+class FacebookLogin(SocialLoginView):
+    adapter_class = FacebookOAuth2Adapter
