1cb2eaadf71d2722db77146b02ce6e4d38a30106
Matheus Fernandes <matpfernandes@gmail.com>
2018-06-26 10:24:30 -0300

    feat(BillingProfile): Add Model and integration with stripe

--- src/billings/models.py
+++ src/billings/models.py
@@ -1,3 +1,41 @@
+from django.conf import settings
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+import stripe
 
-# Create your models here.
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+User = settings.AUTH_USER_MODEL
+
+
+class BillingProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    customer_id = models.CharField(max_length=120, null=True, blank=True)
+
+    def __str__(self):
+        return self.email
+
+
+def billing_profile_created_receiver(sender, instance, *args, **kwargs):
+    if not instance.customer_id and instance.email:
+        customer = stripe.Customer.create(
+            email=instance.email,
+            description="Customer for {}".format(instance.email),
+        )
+        print(customer)
+        instance.customer_id = customer.id
+
+
+pre_save.connect(billing_profile_created_receiver, sender=BillingProfile)
+
+
+def user_created_receiver(sender, instance, created, *args, **kwargs):
+    if created and instance.email:
+        BillingProfile.objects.get_or_create(user=instance, email=instance.email)
+
+
+post_save.connect(user_created_receiver, sender=User)
