efb1f739d3b5c79cb21325c01051bb1a2e8b4d6d
Matheus Fernandes <matpfernandes@gmail.com>
2018-06-26 10:37:26 -0300

    feat(Card): add model and integrations with stripe

--- src/billings/models.py
+++ src/billings/models.py
@@ -19,6 +19,9 @@ class BillingProfile(models.Model):
     def __str__(self):
         return self.email
 
+    def get_cards(self):
+        return self.card_set.all()
+
 
 def billing_profile_created_receiver(sender, instance, *args, **kwargs):
     if not instance.customer_id and instance.email:
@@ -39,3 +42,53 @@ def user_created_receiver(sender, instance, created, *args, **kwargs):
 
 
 post_save.connect(user_created_receiver, sender=User)
+
+
+class CardManager(models.Manager):
+    def all(self, *args, **kwargs):
+        return self.get_queryset().filter(active=True)
+
+    def add_new(self, billing_profile, token):
+        if token:
+            customer = stripe.Customer.retrieve(billing_profile.customer_id)
+            stripe_card_response = customer.sources.create(source=token)
+            new_card = self.model(
+                billing_profile=billing_profile,
+                stripe_id=stripe_card_response.id,
+                brand=stripe_card_response.brand,
+                country=stripe_card_response.country,
+                exp_month=stripe_card_response.exp_month,
+                exp_year=stripe_card_response.exp_year,
+                last4=stripe_card_response.last4
+            )
+            new_card.save()
+            return new_card
+        return None
+
+
+class Card(models.Model):
+    billing_profile = models.ForeignKey(BillingProfile, on_delete=models.CASCADE)
+    stripe_id = models.CharField(max_length=120)
+    brand = models.CharField(max_length=120, null=True, blank=True)
+    country = models.CharField(max_length=120, null=True, blank=True)
+    exp_month = models.IntegerField(null=True, blank=True)
+    exp_year = models.IntegerField(null=True, blank=True)
+    last4 = models.CharField(max_length=4, null=True, blank=True)
+    active = models.BooleanField(default=True)
+    default = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = CardManager()
+
+    def __str__(self):
+        return "{} {}".format(self.brand, self.last4)
+
+
+def new_card_post_save_receiver(sender, instance, created, *args, **kwargs):
+    if instance.default:
+        billing_profile = instance.billing_profile
+        qs = Card.objects.filter(billing_profile=billing_profile).exclude(pk=instance.pk)
+        qs.update(default=False)
+
+
+post_save.connect(new_card_post_save_receiver, sender=Card)
