d0b7957c1dc249a20f7a66552811412af2c34060
Rurik <rurikspin@gmail.com>
2020-01-16 08:10:03 -0300

    chore: rename release echo description to pushing

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,12 +5,12 @@ set -u
 
 case $1 in
     'staging')
-        echo "releasing to STAGING"
+        echo "pushing to STAGING"
         docker tag ${IMAGE_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX_STAGING}:${CIRCLE_SHA1}
         docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX_STAGING}:${CIRCLE_SHA1}
     ;;
     'master')
-        echo "release to PRODUCTION"
+        echo "pushing to PRODUCTION"
         docker tag ${IMAGE_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX_PROD}:${CIRCLE_SHA1}
         docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX_PROD}:${CIRCLE_SHA1}
     ;;
