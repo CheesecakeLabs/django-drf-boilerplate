4975005e53c15f8e5c9c82731c059bfff6269b87
Rurik <rurikspin@gmail.com>
2020-01-27 13:05:26 -0300

    chore: refact deploy commands on circleci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,27 @@
 set -e
 set -u
 
+SERVICE_NAME=""
+TAG=""
 case $1 in
-    'staging')
+    "lab")
+        echo "deploying to LAB"
+        SERVICE_NAME=${AWS_SERVICE_NAME_LAB}
+        TAG="lab"
+    ;;
+    "staging")
         echo "deploying to STAGING"
-        ecs-deploy -c ${AWS_CLUSTER_NAME} -n ${AWS_SERVICE_NAME_STAGING} -i ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX_STAGING}:${CIRCLE_SHA1} -r ${AWS_DEFAULT_REGION} --timeout 108000
+        SERVICE_NAME=${AWS_SERVICE_NAME_STAGING}
+        TAG="staging"
     ;;
     *)
         echo "deploying to PRODUCTION"
-        ecs-deploy -c ${AWS_CLUSTER_NAME} -n ${AWS_SERVICE_NAME_PROD} -i ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX}:${CIRCLE_SHA1} -r ${AWS_DEFAULT_REGION} --timeout 108000
+        SERVICE_NAME=${AWS_SERVICE_NAME_PROD}
+        TAG=${CIRCLE_TAG}
+        docker tag ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX}:${CIRCLE_TAG}
+        docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX}:${CIRCLE_TAG}
     ;;
 esac
+
+# deploying to AWS ECS
+ecs-deploy -c ${AWS_CLUSTER_NAME} -n ${SERVICE_NAME} -i ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX}:${TAG} -r ${AWS_DEFAULT_REGION} --timeout 108000
