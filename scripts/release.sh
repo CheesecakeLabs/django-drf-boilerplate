2c6a042dca2199242e2eca84a5efb517b51d3e23
Rurik <rurikspin@gmail.com>
2020-01-26 21:27:13 -0300

    chore: refact relase command

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,18 +4,9 @@ set -e
 set -u
 
 case $1 in
-    'staging')
-        echo "pushing to STAGING"
-        docker tag ${IMAGE_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX_STAGING}:${CIRCLE_SHA1}
-        docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX_STAGING}:${CIRCLE_SHA1}
-    ;;
-    'master')
-        echo "pushing to PRODUCTION"
-        docker tag ${IMAGE_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX_PROD}:${CIRCLE_SHA1}
-        docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX_PROD}:${CIRCLE_SHA1}
-    ;;
     *)
-        echo "release not defined"
-        exit 1
+        echo "release ${CIRCLECI_BRANCH}"
+        docker tag ${IMAGE_NAME}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX}:${CIRCLE_SHA1}
+        docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX}:${CIRCLE_SHA1}
     ;;
 esac
