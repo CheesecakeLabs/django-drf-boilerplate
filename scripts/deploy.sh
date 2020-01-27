5b4ae6752a98c496be155afd34fd3d8e34b3f84b
Rurik <rurikspin@gmail.com>
2020-01-26 21:32:46 -0300

    chore: refact deploy command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,10 +6,10 @@ set -u
 case $1 in
     'staging')
         echo "deploying to STAGING"
-        ecs-deploy -c ${AWS_CLUSTER_NAME} -n ${AWS_SERVICE_NAME} -i ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX_STAGING}:${CIRCLE_SHA1} -r ${AWS_DEFAULT_REGION}
+        ecs-deploy -c ${AWS_CLUSTER_NAME} -n ${AWS_SERVICE_NAME_STAGING} -i ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX_STAGING}:${CIRCLE_SHA1} -r ${AWS_DEFAULT_REGION} --timeout 108000
     ;;
     *)
         echo "deploying to PRODUCTION"
-        ecs-deploy -c ${AWS_CLUSTER_NAME} -n ${AWS_SERVICE_NAME} -i ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX_PROD}:${CIRCLE_SHA1} -r ${AWS_DEFAULT_REGION}
+        ecs-deploy -c ${AWS_CLUSTER_NAME} -n ${AWS_SERVICE_NAME_PROD} -i ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${AWS_RESOURCE_NAME_PREFIX}:${CIRCLE_SHA1} -r ${AWS_DEFAULT_REGION} --timeout 108000
     ;;
 esac
