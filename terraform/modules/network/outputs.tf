3e5b7517ed50804553e45dfb5209795e2524bf8f
Bernardo Smaniotto <be.smaniotto@gmail.com>
2020-02-03 13:59:54 -0300

    feat(terraform): move availability zones to tf variables and rename subnets

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -2,26 +2,26 @@ output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
-output "private_subnet_id" {
-  value = aws_subnet.private_subnet.id
+output "private_subnet_1_id" {
+  value = aws_subnet.private_subnet_1.id
 }
 
 output "private_subnet_2_id" {
   value = aws_subnet.private_subnet_2.id
 }
 
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
+output "public_subnet_1_id" {
+  value = aws_subnet.public_subnet_1.id
 }
 
 output "public_subnet_2_id" {
   value = aws_subnet.public_subnet_2.id
 }
 
-output "public_secutity_group_id" {
-  value = aws_security_group.public_secutity_group.id
+output "public_security_group_id" {
+  value = aws_security_group.public_security_group.id
 }
 
-output "private_secutity_group_id" {
-  value = aws_security_group.private_secutity_group.id
+output "private_security_group_id" {
+  value = aws_security_group.private_security_group.id
 }
