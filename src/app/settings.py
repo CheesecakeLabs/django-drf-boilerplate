7d608faae74f5df539b1a66a6c6ff383b3f6c81e
Frederico Jordan <fredericojordan@gmail.com>
2020-02-06 11:00:26 -0300

    style: Run Black on src/

--- src/app/settings.py
+++ src/app/settings.py
@@ -156,15 +156,15 @@ if env.bool("ENABLE_SENTRY", False):
 # https://django-versatileimagefield.readthedocs.io/en/latest/installation.html#settings
 
 VERSATILEIMAGEFIELD_SETTINGS = {
-    'cache_length': 2592000,
-    'cache_name': 'versatileimagefield_cache',
-    'jpeg_resize_quality': 70,
-    'sized_directory_name': '__sized__',
-    'filtered_directory_name': '__filtered__',
-    'placeholder_directory_name': '__placeholder__',
-    'create_images_on_demand': True,
-    'image_key_post_processor': None,
-    'progressive_jpeg': False
+    "cache_length": 2592000,
+    "cache_name": "versatileimagefield_cache",
+    "jpeg_resize_quality": 70,
+    "sized_directory_name": "__sized__",
+    "filtered_directory_name": "__filtered__",
+    "placeholder_directory_name": "__placeholder__",
+    "create_images_on_demand": True,
+    "image_key_post_processor": None,
+    "progressive_jpeg": False,
 }
 
 # The rendition key sets will be used if 'create_images_on_demand' is set to False
